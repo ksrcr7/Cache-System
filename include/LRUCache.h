4302cf80dcfa512c5c7073e38e03e7b4fa12b0ad
ksrcr7 <kasrabakhshaei25@gmail.com>
2025-12-16 20:33:46 +0330

    chore: stop tracking cmake-build-debug

---
+++ include/LRUCache.h
@@ -0,0 +1,45 @@
+#ifndef CACHE_SYSTEM_LRUCACHE_H
+#define CACHE_SYSTEM_LRUCACHE_H
+
+#include <unordered_map>
+#include <list>
+
+
+class LRUCache{
+public:
+    LRUCache(int);
+    bool get(int,int&);
+    void put(int,int);
+    int size()const;
+    int capacity()const;
+
+private:
+    int capacity_;
+    using Node = std::pair<int,int>;
+    using ListIt = std::list<Node>::iterator;
+    std::list<Node> cacheList;
+    std::unordered_map<int, ListIt> HashMap;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //CACHE_SYSTEM_LRUCACHE_H
--- src/LRUCache.cpp
+++ src/LRUCache.cpp
@@ -1,1 +1,1 @@
-
+#include "LRUCache.h"
