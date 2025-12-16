2d027aee6cd8aceb1a12e8f9cded1698a2cdd283
ksrcr7 <kasrabakhshaei25@gmail.com>
2025-12-16 21:51:52 +0330

    feat: implement LRUCache get

--- src/LRUCache.cpp
+++ src/LRUCache.cpp
@@ -1,1 +1,14 @@
 #include "LRUCache.h"
+
+bool LRUCache::get(int key, int &outValue) {
+    auto find = HashMap.find(key);
+    if(find == HashMap.end())
+        return false;
+
+    auto it = find->second;
+    outValue = it->second;
+
+    cacheList.splice(cacheList.begin(),cacheList,it);
+    return true;
+
+}
