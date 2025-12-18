5124800ae24a39d24d3dd3e08bff4cde2bc317b5
ksrcr7 <kasrabakhshaei25@gmail.com>
2025-12-18 15:04:51 +0330

    "feat: implement LRUCache put with eviction"

--- src/LRUCache.cpp
+++ src/LRUCache.cpp
@@ -21,3 +21,22 @@ LRUCache::LRUCache(int capacity) {
     capacity_ = capacity;
 
 }
+
+void LRUCache::put(int key, int value) {
+    auto it = HashMap.find(key);
+    if(it != HashMap.end()){
+        auto update = it->second;
+        update->second = value;
+        cacheList.splice(cacheList.begin(),cacheList,update);
+        return;
+    }
+    if(cacheList.size() == capacity_){
+        int oldKey = cacheList.back().first;
+        HashMap.erase(oldKey);
+        cacheList.pop_back();
+    }
+
+    cacheList.emplace_front(key,value);
+    HashMap[key] = cacheList.begin();
+
+}
